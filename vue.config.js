e671b016fe0afb932c8e35916c0a9596c3df265a
kgB742 <114336498+kgB742@users.noreply.github.com>
2025-01-03 23:08:15 +0100

    chore(setup): all the rest

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: [
+      'vue.kundk.local'
+    ],
+    client: {
+      webSocketURL: 'ws://0.0.0.0:8080/ws', // Configure WebSocket URL
+    },
+    webSocketServer: 'ws', // Use WebSocket server
+  }
 })
